5e0139a1a5292a1fd64c7157b32895460494f3ad
Vuthithieulinh <vuthithieulinh@gmail.com>
2024-08-11 01:48:38 +0700

    Add Topic_02

---
+++ selenium-java-testng/selenium/webdriver/Add_Topic_02.java
@@ -0,0 +1,13 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Add_Topic_02 {
+    WebDriver driver;
+
+    @Test
+    public void TC_01() {
+    }
+}
